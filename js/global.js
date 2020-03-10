d887b8d8ae628c1bf1c016b8550897bf75da942a
Pedro Luis Arend Gonçalves <50812580+pedrolag@users.noreply.github.com>
2020-03-10 13:05:27 -0300

    updated - global.js
    Criada a lógica para aplicar o efeito "fade-in" apenas quando a página "index.html" estiver totalmente carregada.

--- js/global.js
+++ js/global.js
@@ -1,13 +1,20 @@
 
 $(document).ready(function() {
     progressBar();
+    showWelcomeMessage();
 });
 
-$(window).scroll(function () {
-    
-    console.log('teste');
-
-});
+/**
+ *  Aplica o efeito "fade in" na mensagem de bem-vindo
+ *  na secção "hello"
+ */
+function showWelcomeMessage() {
+    $("h1.intro").addClass("fadeIn delay-1s animated");
+    $("h1.intro span").addClass("fadeIn delay-2s animated");
+    $("h1.first-name").addClass("fadeIn delay-3s animated");
+    $("h1.middle-name").addClass("fadeIn delay-3s animated");
+    $("h1.last-name").addClass("fadeIn delay-3s animated");
+}
 
 /**
  *  Atribui a largura desejada nas barras de progresso
