b8315292b8a328dfbf4d253907dda03b2b0a0562
Pedro Goncalves <pedro@unimestre.com>
2020-03-11 10:22:40 -0300

    updated - global.js - Adicionado o efeito 'fade-in' no botão da secção 'hello'.

--- js/global.js
+++ js/global.js
@@ -9,11 +9,12 @@ $(document).ready(function() {
  *  na secção "hello"
  */
 function showWelcomeMessage() {
-    $("h1.intro").addClass("fadeIn delay-1s animated");
-    $("h1.intro span").addClass("fadeIn delay-2s animated");
-    $("h1.first-name").addClass("fadeIn delay-3s animated");
-    $("h1.middle-name").addClass("fadeIn delay-3s animated");
-    $("h1.last-name").addClass("fadeIn delay-3s animated");
+    $("#hello h1.intro").addClass("fadeIn delay-1s animated");
+    $("#hello h1.intro span").addClass("fadeIn delay-2s animated");
+    $("#hello h1.first-name").addClass("fadeIn delay-3s animated");
+    $("#hello h1.middle-name").addClass("fadeIn delay-3s animated");
+    $("#hello h1.last-name").addClass("fadeIn delay-3s animated");
+    $("#hello button").addClass("fadeIn delay-4s animated");
 }
 
 /**
