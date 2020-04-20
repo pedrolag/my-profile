62ce177abc0dbf995e6c01ec4370588810f36594
Pedro Luis Arend Gonçalves <50812580+pedrolag@users.noreply.github.com>
2020-04-19 21:33:08 -0300

    updated - global.js

--- js/global.js
+++ js/global.js
@@ -1,21 +1,21 @@
 
 $(document).ready(function() {
     progressBar();
-    showWelcomeMessage();
+    // showWelcomeMessage();
 });
 
 /**
  *  Aplica o efeito "fade in" na mensagem de bem-vindo
  *  na secção "hello"
  */
-function showWelcomeMessage() {
-    $("#hello h1.intro").addClass("fadeIn delay-1s animated");
-    $("#hello h1.intro span").addClass("fadeIn delay-2s animated");
-    $("#hello h1.first-name").addClass("fadeIn delay-3s animated");
-    $("#hello h1.middle-name").addClass("fadeIn delay-3s animated");
-    $("#hello h1.last-name").addClass("fadeIn delay-3s animated");
-    $("#hello button").addClass("fadeIn delay-4s animated");
-}
+// function showWelcomeMessage() {
+//     $("#hello h1.intro").addClass("fadeIn delay-1s animated");
+//     $("#hello h1.intro span").addClass("fadeIn delay-2s animated");
+//     $("#hello h1.first-name").addClass("fadeIn delay-3s animated");
+//     $("#hello h1.middle-name").addClass("fadeIn delay-3s animated");
+//     $("#hello h1.last-name").addClass("fadeIn delay-3s animated");
+//     $("#hello button").addClass("fadeIn delay-4s animated");
+// }
 
 /**
  *  Atribui a largura desejada nas barras de progresso
