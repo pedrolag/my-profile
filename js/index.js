e33ca59ddbb4a4cd0efbcca264024a59b5fa3caa
Pedro Gonçalves <pedro@unimestre.com>
2021-01-14 14:15:42 -0300

    adicionado efeito de ondas

--- js/index.js
+++ js/index.js
@@ -18,4 +18,20 @@ new Vue({
     mounted() {
         this.getArrSections();
     }
-})
+});
+
+var myWave = $('#wave-1').wavify({
+    height: 60,
+    bones: 5,
+    amplitude: 40,
+    color: '#30475e',
+    speed: .25
+});
+
+var myWave2 = $('#wave-2').wavify({
+    height: 60,
+    bones: 3,
+    amplitude: 38,
+    color: '#4b6a8a',
+    speed: .20
+});
